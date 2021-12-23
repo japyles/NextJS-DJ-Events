3f0b911219ced9c95ad7bad05b35d8024b1d113f
John Pyles <pylesjohn@gmail.com>
2021-12-23 00:21:18 -0500

    created Layout, Header and Footer components and styling. Enabled SASS

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,7 +1,9 @@
+import Layout from '../../components/Layout'
+
 export default function AddEventPate() {
     return (
-        <div>
+        <Layout title='Add New Event'>
             <h1>Add Event</h1>
-        </div>
+        </Layout>
     )
 }
